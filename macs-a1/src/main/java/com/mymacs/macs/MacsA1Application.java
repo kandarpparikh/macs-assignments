ca76e66109e17b40b26f597d5efc3c1fadd46d95
Kandarp Parikh <kn616924@dal.ca>
2021-10-05 03:39:42 +0530

    testing CI .yml file

---
+++ macs-a1/src/main/java/com/mymacs/macs/MacsA1Application.java
@@ -0,0 +1,13 @@
+package com.mymacs.macs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacsA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacsA1Application.class, args);
+	}
+
+}
---
+++ macs-a1/src/test/java/com/mymacs/macs/MacsA1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymacs.macs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacsA1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
